365dea573035cb1b825789ed63cd11b0a7e933c4
Gennadiy Andriyovych Byerlov <BXrlo_lk05@student.itstep.org>
2023-05-21 13:00:31 +0300

    Update DZ.cpp

--- DZ/DZ.cpp
+++ DZ/DZ.cpp
@@ -92,5 +92,107 @@ int main() {
 	else {
 		cout << "The plane will not be able to overcome this route without additional refueling!" << endl;
 	}
+
+	bool isLeapYear(int year) {
+	    return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+	}
+
+	int getDaysInMonth(int month, int year) {
+	    switch (month) {
+		case 2:
+		    return isLeapYear(year) ? 29 : 28;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+		    return 30;
+		default:
+		    return 31;
+	    }
+	}
+
+	int main() {
+	    int day1, month1, year1;
+	    int day2, month2, year2;
+
+	    cout << "Enter the first date (day month year): ";
+	    cin >> day1 >> month1 >> year1;
+
+	    cout << "Enter the second date (day month year): ";
+	    cin >> day2 >> month2 >> year2;
+
+	    int days = 0;
+
+	    if (year2 > year1 || (year2 == year1 && month2 > month1) || (year2 == year1 && month2 == month1 && day2 > day1)) {
+		while (year1 < year2 || month1 < month2 || day1 < day2) {
+		    days++;
+		    day1++;
+
+		    if (day1 > getDaysInMonth(month1, year1)) {
+			day1 = 1;
+			month1++;
+
+			if (month1 > 12) {
+			    month1 = 1;
+			    year1++;
+			}
+		    }
+		}
+	    }
+
+	    cout << "Number of days between the two dates: " << days << endl;
+		    double sales1, sales2, sales3;
+    double commissionRate = 0.0;
+    double salary1, salary2, salary3;
+
+    cout << "Enter sales for manager 1: ";
+    cin >> sales1;
+    cout << "Enter sales for manager 2: ";
+    cin >> sales2;
+    cout << "Enter sales for manager 3: ";
+    cin >> sales3;
+
+    // Розрахунок зарплати для кожного менеджера
+    if (sales1 <= 500) {
+        commissionRate = 0.03;
+    } else if (sales1 <= 1000) {
+        commissionRate = 0.05;
+    } else {
+        commissionRate = 0.08;
+    }
+    salary1 = 200 + (sales1 * commissionRate);
+
+    if (sales2 <= 500) {
+        commissionRate = 0.03;
+    } else if (sales2 <= 1000) {
+        commissionRate = 0.05;
+    } else {
+        commissionRate = 0.08;
+    }
+    salary2 = 200 + (sales2 * commissionRate);
+
+    if (sales3 <= 500) {
+        commissionRate = 0.03;
+    } else if (sales3 <= 1000) {
+        commissionRate = 0.05;
+    } else {
+        commissionRate = 0.08;
+    }
+    salary3 = 200 + (sales3 * commissionRate);
+
+    double maxSalary = max(max(salary1, salary2), salary3);
+
+    if (maxSalary == salary1) {
+        salary1 += 200;
+    } else if (maxSalary == salary2) {
+        salary2 += 200;
+    } else {
+        salary3 += 200;
+    }
+
+
+    cout << "Salary for manager 1: $" << salary1 << endl;
+    cout << "Salary for manager 2: $" << salary2 << endl;
+    cout << "Salary for manager 3: $" << salary3 << endl;
 	return 0;
 }
